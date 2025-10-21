e98f7ed6378fad8146301f27e58df182623f9ed4
Felipe Castro <carloscastro1860@gmail.com>
2025-10-21 15:39:37 -0500

    fix(front): config and dependency

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <h1>Taskly</h1>
       <div className="card bg-base-100 w-96 shadow-sm">
         <figure>
           <img
