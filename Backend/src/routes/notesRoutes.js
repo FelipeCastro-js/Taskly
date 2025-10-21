cd675deeffb7551f2e05fb7e262cbd6bcae3816f
Felipe Castro <carloscastro1860@gmail.com>
2025-10-21 14:54:08 -0500

    feat(db): add and config http for database and App

--- Backend/src/routes/notesRoutes.js
+++ Backend/src/routes/notesRoutes.js
@@ -1,37 +1,91 @@
 import express from "express";
+import Note from "../models/noteModel.js";
 
 const router = express.Router();
 
 // GET
-router.get("/", () => {
-  console.log("Enviando una nota");
+router.get("/", async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error al obtener las notas", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  console.log(`Id de la nota ${id}`);
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const note = await Note.findById(id);
+
+    if (!note) return res.status(404).json({ message: "Nota no encontrada" });
+
+    res.status(200).json(note);
+  } catch (error) {
+    console.error("Error al obtener nota por id ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 // POST
-router.post("/", (req, res) => {
-  const { title, description } = req.body;
-  console.log(title, description);
+router.post("/", async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const note = new Note({ title, description });
+
+    // Save note database
+    const savedNote = await note.save();
+    if (savedNote) {
+      res
+        .status(201)
+        .json({ message: "Nota creada correctamente", note: savedNote });
+    }
+  } catch (error) {
+    console.log("Error al crear la nota", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 // DELETE
 
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("Eliminar la nota");
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const note = await Note.findByIdAndDelete(id);
+    if (!note)
+      return res
+        .status(404)
+        .json({ message: "No se encontro la nota que desea eliminar" });
+    res.status(200).json({ message: "Nota exitosamente eliminada" });
+  } catch (error) {
+    console.log("Error al crear la nota", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 // UPDATE
 
-router.put("/:id", (req, res) => {
-  const id = req.params.id;
-  const editData = req.body;
-
-  console.log(editData);
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { title, description } = req.body;
+    const updateNote = await Note.findByIdAndUpdate(
+      id,
+      { title, description },
+      { new: true }
+    );
+    if (!updateNote)
+      return res
+        .status(404)
+        .json({ message: "No se pudo actualizar correctamente" });
+    res
+      .status(200)
+      .json({ message: "Nota actualizada con exito", note: updateNote });
+  } catch (error) {
+    console.log("Error al actualizar", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 export default router;
